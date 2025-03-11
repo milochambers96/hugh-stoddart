ca532ea393cde664d134eff3e79d75f52ac21ad2
Milo Chambers <chambersmilo@gmail.com>
2025-03-11 16:37:16 +0000

    style(home): update homepage to include screenwriter info

--- src/components/HomePage/Home.tsx
+++ src/components/HomePage/Home.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Home = () => {
   const [text, setText] = useState("");
@@ -44,18 +44,19 @@ const Home = () => {
       <h1 className="text-5xl sm:text-7xl mb-2 text-center text-hs-title font-bold  font-title">
         Hugh Stoddart
       </h1>
+      <h2 className="text-4xl mt-4 text-hs-subtitle font-body">Screenwriter</h2>
 
-      <div className="w-24 h-1 bg-hs-accent 0 mt-4 mb-6"></div>
+      <div className="w-24 h-1 bg-hs-accent 0 mt-6 mb-4"></div>
 
       <div className="h-16 flex items-center justify-center">
-        <p className="text-xl sm:text-2xl text-hs-accent font-medium">
+        <p className="text-xl sm:text-xl text-hs-accent font-medium">
           <span className="inline-block min-h-8 border-r-4 border-amber-500 pr-1 animate-pulse font-interactive">
             {text}
           </span>
         </p>
       </div>
 
-      <div className="flex space-x-4 mt-8 font-interactive">
+      {/* <div className="flex space-x-4 mt-8 font-interactive">
         <Link to="/Writings">
           <button className="px-6 py-3 bg-hs-accent/50 hover:bg-hs-button text-hs-button-text font-bold rounded transition-colors duration-300">
             WRITTEN WORKS
@@ -67,7 +68,7 @@ const Home = () => {
             CONTACT HUGH
           </button>
         </Link>
-      </div>
+      </div> */}
     </section>
   );
 };

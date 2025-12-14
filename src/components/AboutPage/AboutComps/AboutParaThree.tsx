7eb89961bafcf29d7d86bb8ce4725350b7189dd8
Milo Chambers <chambersmilo@gmail.com>
2025-12-14 15:59:01 +0000

    update about page links

--- src/components/AboutPage/AboutComps/AboutParaThree.tsx
+++ src/components/AboutPage/AboutComps/AboutParaThree.tsx
@@ -3,7 +3,7 @@ const AboutParaThree = () => {
     <p>
       He co-founded{" "}
       <span className="italic font-bold text-hs-link hover:text-hs-link-hover transition-colors duration-500">
-        <a href="http://localhost:5173/Writings/Prison">Not Shut Up</a>
+        <a href="https://hughstoddart.co.uk/Writings/Prison">Not Shut Up</a>
       </span>{" "}
       in 2003, and alongside his own work, became its managing editor until
       2012, establishing it as a national magazine to showcase the writing and
